da87267b91e0d1c02b7f3cd6d48cf8ad534e271f
myyrakle <sssang97@naver.com>
2024-03-11 00:08:08 +0900

    [#3] size 추출 기능 구현

--- src/action/dump.rs
+++ src/action/dump.rs
@@ -1,5 +1,19 @@
 use crate::command::dump::ConfigOptions;
 
 pub fn run(option: ConfigOptions) {
-    println!("run dump");
+    let byte_size = if option.size.ends_with("g") {
+        let giga_size = option.size.trim_end_matches("g").parse::<u64>().unwrap();
+
+        giga_size * 1024 * 1024 * 1024
+    } else if option.size.ends_with("m") {
+        let mega_size = option.size.trim_end_matches("m").parse::<u64>().unwrap();
+
+        mega_size * 1024 * 1024
+    } else if option.size.ends_with("k") {
+        let killo_size = option.size.trim_end_matches("k").parse::<u64>().unwrap();
+
+        killo_size * 1024
+    } else {
+        option.size.parse::<u64>().unwrap()
+    };
 }
